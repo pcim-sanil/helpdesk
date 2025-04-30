338ed07af11abefd129d6a7dfffb1f9b68d26e12
web.developer.sanil@gmail.com <web.developer.sanil@gmail.com>
2025-04-30 12:06:54 +0000

    pm2 config to start queue worker

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'laravel-queue',
+      script: 'artisan',
+      args: 'queue:work database --sleep=3 --tries=3 --timeout=600',
+      interpreter: '/usr/bin/php',
+      cwd: '/var/www/html/helpdesk',
+      instances: 2,
+      autorestart: true,
+      max_memory_restart: '512M',
+      kill_timeout: 30000,
+      watch: false,
+      uid: 'nginx',
+      gid: 'nginx'
+    },
+  ],
+};
