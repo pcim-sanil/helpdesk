d1d7d54b2fdc4aa8bce9dc80b65787df884fcdc6
web.developer.sanil@gmail.com <web.developer.sanil@gmail.com>
2025-05-02 08:42:26 +0000

    only run one queue worker

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       args: 'queue:work database --sleep=3 --tries=3 --timeout=600',
       interpreter: '/usr/bin/php',
       cwd: '/var/www/html/helpdesk',
-      instances: 2,
+      instances: 1,
       autorestart: true,
       max_memory_restart: '512M',
       kill_timeout: 30000,
