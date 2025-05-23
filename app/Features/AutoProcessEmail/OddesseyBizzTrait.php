96e44773fd41fd53be2183e13b5ddb3e9c04fa22
web.developer.sanil@gmail.com <web.developer.sanil@gmail.com>
2025-05-24 00:45:28 +1000

    Fix 500 server errors in jobs.

--- app/Features/AutoProcessEmail/OddesseyBizzTrait.php
+++ app/Features/AutoProcessEmail/OddesseyBizzTrait.php
@@ -6,65 +6,71 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\RequestException;
 use App\Features\AutoProcessEmail\Data\AutoProcessedEmailData;
 
-trait OddesseysmsTrait
+trait OddesseyBizzTrait
 {
     abstract public function getBaseUri(): string;
+    abstract public function getToken(): string;
 
+    /**
+     * POST http://cl1.oddesseybizz.nl/gogogy/CZ2/customercare_lookup.php?msisdn=00xxxx&token=67890
+     * Sample response:{"active":true,"msisdn":"00420722109300","activated":"1745201345","stopped":"","operator":"23002","opt-out":"unsubscribe"},
+     * Sample response:{"active":false,"msisdn":"00420736166731","activated":"1745202969","stopped":"1746429802","operator":"23003","opt-out":"unsubscribe"}}
+     */
     public function getSubscriptions(string $mobileNumber): array
     {
-        try{
+        try {
             $response = Http::timeout(300)
                 ->retry(2, 100)
-                ->withHeaders([
-                    'Content-Type' => 'application/json',
+                ->withOptions([
+                    'query' => [
+                        'msisdn' => $this->normalize($mobileNumber),
+                        'token'  => $this->getToken(),
+                    ],
                 ])
-                ->post('https://portal.telcosupport.com/phpinfo.php', [
-                    'token' => 'nakuit',
-                    'endpoint' => $this->getBaseUri() . "/lookup?msisdn=" . $this->normalize($mobileNumber),
-                    'method' => 'POST',
-                ])
+                ->post($this->getBaseUri() . "/customercare_lookup.php")
                 ->throw();
 
             return $response->json();
-        } catch(RequestException $e) {
+        } catch (RequestException $e) {
             $status = $e->response->status();
             $body = $e->response?->json();
 
-            if(is_array($body)) {
+            if (is_array($body)) {
                 return $body;
             }
 
             throw $e;
         }
     }
-    
+
     public function unsubscribe(string $mobileNumber): array
     {
-        try{
+        try {
             $response = Http::timeout(300)
                 ->retry(2, 100)
-                ->withHeaders([
-                'Content-Type' => 'application/json',
-            ])
-            ->post('https://portal.telcosupport.com/phpinfo.php', [
-                'token' => 'nakuit',
-                'endpoint' => $this->getBaseUri() . "/unsubscribe?msisdn=" . $this->normalize($mobileNumber),
-                'method' => 'POST',
-            ])
-            ->throw();
+                ->withOptions([
+                    'query' => [
+                        'msisdn' => $this->normalize($mobileNumber),
+                        'token'  => $this->getToken(),
+                    ],
+                ])
+                ->post($this->getBaseUri() . "/customercare_unsubscribe.php")
+                ->throw();
 
             return $response->json();
-        } catch(RequestException $e) {
+        } catch (RequestException $e) {
             $status = $e->response->status();
             $body = $e->response?->json();
 
-            if(is_array($body)) {
+            if (is_array($body)) {
                 return $body;
             }
 
             throw $e;
         }
     }
+
+
 
     /**
      * Fetch subscriptions for mobile numbers.
@@ -80,12 +86,11 @@ trait OddesseysmsTrait
          */
         foreach ($mobileNumbers as $mobileNumber) {
             $subscription = $this->getSubscriptions($mobileNumber);
-            $success = $subscription['success'] ?? false;
-            $status = $subscription['status'] ?? '';
-            $error = $subscription['error'] ?? '';
-            $unsubscribedAt = $subscription['unsubscribedAt'] ?? '';
-            
-            if (in_array($success, [true, 'true'], true) && (trim($status) === 'active' || empty($unsubscribedAt))) { 
+            $active = $subscription['active'] ?? false;
+            $activated = $subscription['activated'] ?? '';
+            $stopped = $subscription['stopped'] ?? '';
+
+            if (in_array($active, [true, 'true'], true) || (empty($stopped) && !empty($activated))) {
                 // Mobile number with active subscription.
                 $autoProcessedEmailData->setHasActiveSubscription(true);
                 $autoProcessedEmailData->updateMobileNumberWithActiveSubscription($mobileNumber);
@@ -101,16 +106,14 @@ trait OddesseysmsTrait
      * Unsubscribe from mobile numbers.
      *
      * @param AutoProcessedEmailData $autoProcessedEmailData
-     * @param array $mobileNumbersWithActiveSubscription
+     * @param array $mobileNumbers
      * @return AutoProcessedEmailData
      */
     public function unsubscribeFromMobileNumbers(AutoProcessedEmailData $autoProcessedEmailData, array $mobileNumbersWithActiveSubscription): AutoProcessedEmailData
     {
         foreach ($mobileNumbersWithActiveSubscription as $mobileNumberWithActiveSubscription) {
             $unsubscribed = $this->unsubscribe($mobileNumberWithActiveSubscription);
-
             $success = $unsubscribed['success'] ?? false;
-            $error = $unsubscribed['error'] ?? null;
 
             if (in_array($success, [true, 'true'], true)) {
                 $autoProcessedEmailData->setWasUnsubscribed(true);
@@ -122,5 +125,4 @@ trait OddesseysmsTrait
 
         return $autoProcessedEmailData;
     }
-    
 }
